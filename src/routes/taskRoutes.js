0936a68ad10ac33c00804737ca59e466849dbf2a
fang69x <109065342+fang69x@users.noreply.github.com>
2025-03-06 13:50:57 +0530

    all routes working perfectly

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express'
-import { createTask } from '../controller/taskController.js'
+import { createTask, deleteTask, getAllTask, getTaskById, updateTask } from '../controller/taskController.js'
 const router=express.Router();
 
 router.post('/tasks',createTask);
+router.get('/tasks',getAllTask);
+router.get('/tasks/:id',getTaskById);
+router.put('/tasks/:id',updateTask);
+router.delete('/tasks/:id',deleteTask);
+
 
 
 export default router;
