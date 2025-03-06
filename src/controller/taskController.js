bb03b871dd78385932269c13ab7d857ffabe6a0f
fang69x <109065342+fang69x@users.noreply.github.com>
2025-03-06 13:50:47 +0530

    CRUD controllers written and tested

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -15,3 +15,71 @@ export const createTask=async(req,res)=>{
         });
     }
 }
+// get all task
+export const getAllTask=async(req,res)=>{
+    try {
+  const tasks= await Task.find();
+  res.status(200).json(tasks);      
+    } catch (error) {
+        res.status(500).json({
+            message:"Error fetching tasks",
+            error:error.message
+        })
+    }
+};
+//get a single task by ID
+
+export const getTaskById=async(req,res)=>{
+    try {
+        const task =await Task.findById(req.params.id);
+        if(!task){
+            return res.status(404).json({
+                message:"Task not found",
+                error:error.message
+            })
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({
+            message:"Error fetching task",
+            error:error.message
+        })
+    }
+};
+// update a task
+export const updateTask=async(req,res)=>{
+    try {
+        const updatedTask =await Task.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!updatedTask){
+      return res.status(404).json({message:"Task not found"});
+        }
+        res.status(200).json({
+            updatedTask
+        })
+    } catch (error) {
+        res.status(500).json({
+            message:"Error updating task",
+            error:error.message
+        })
+    }
+};
+// delete a task
+
+export const deleteTask=async(req,res)=>{
+try {
+    const deletedTask=await Task.findByIdAndDelete(req.params.id);
+    if(!deletedTask)
+    {
+        return res.status(404).json({
+            message:"Task not found",
+        })
+    }
+    res.status(200).json({message:"Task deleted successfully"});
+} catch (error) {
+    res.status(500).json({
+        message:"Error deleting task",
+        error:error.message
+    })
+    
+}
+}
