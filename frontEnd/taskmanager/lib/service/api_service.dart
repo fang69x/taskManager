c5810d7e94ecc11890878e9f7be9708313ffa0af
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-10 21:58:12 +0530

    api service finished

--- frontEnd/taskmanager/lib/service/api_service.dart
+++ frontEnd/taskmanager/lib/service/api_service.dart
@@ -57,5 +57,127 @@ class ApiService {
       return {'error ': e.toString()};
     }
   }
+
   // User Login
+  Future<Map<String, dynamic>> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/auth/login'),
+        headers: {'Content-type': 'application/json'},
+        body: json.encode({
+          'email': email,
+          'password': password,
+        }),
+      );
+      var data = json.decode(response.body);
+      if (response.statusCode == 200) {
+        await setToken(data['token']);
+      }
+      return data;
+    } catch (e) {
+      return {'error': e.toString()};
+    }
+  }
+
+  //get user profile
+  Future<Map<String, dynamic>> getUserProfile() async {
+    try {
+      final token = await getToken();
+      final response = await http.get(
+        Uri.parse('$baseUrl/auth/profile'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+      return json.decode(response.body);
+    } catch (e) {
+      return {'error': e.toString()};
+    }
+  }
+
+// get all tasks
+  Future<List<dynamic>> getAllTasks() async {
+    try {
+      final token = await getToken();
+      final response = await http.get(
+        Uri.parse('$baseUrl/tasks/list'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
+  }
+  //create task
+
+  Future<Map<String, dynamic>> createTask({
+    required String title,
+    required String description,
+  }) async {
+    try {
+      final token = await getToken();
+      final response = await http.post(
+        Uri.parse('$baseUrl/tasks/create'),
+        headers: {
+          'Content-Type': 'appilcation/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: json.encode({
+          'title': title,
+          'description': description,
+        }),
+      );
+      return json.decode(response.body);
+    } catch (e) {
+      return {'error': e.toString()};
+    }
+  }
+
+//update task
+  Future<Map<String, dynamic>> updateTask({
+    required String id,
+    required Map<String, dynamic> taskData,
+  }) async {
+    try {
+      final token = await getToken();
+      final response = await http.put(
+        Uri.parse('$baseUrl/update/$id'),
+        headers: {
+          'Content-Type': 'appilcation/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: json.encode(taskData),
+      );
+      return json.decode(response.body);
+    } catch (e) {
+      return {'error': e.toString()};
+    }
+  }
+
+//Delete Task
+
+  Future<Map<String, dynamic>> deleteTask(String id) async {
+    try {
+      final token = await getToken();
+      final response =
+          await http.delete(Uri.parse('$baseUrl/tasks/remove/$id'), headers: {
+        'Content-Type': 'appilcation/json',
+        'Authorization': 'Bearer $token',
+      });
+      return json.decode(response.body);
+    } catch (e) {
+      return {'error': e.toString()};
+    }
+  }
 }
